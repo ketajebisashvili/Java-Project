7f60b9c096b109ac05646740aa6e905e5c66826a
Benedek Maveev <[bematv@ttu.ee]>
2022-03-17 13:49:31 +0200

    Create MaxOfOddCalculatorUtil, and test integers

---
+++ src/test/java/ee/taltech/calculator/util/MaxOfOddCalculatorUtilTest.java
@@ -0,0 +1,26 @@
+package ee.taltech.calculator.util;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MaxOddCalculatorUtilTest {
+
+    @Test
+    @DisplayName("MaxOdd result is null or empty")
+    void maxOddIsNullOrEmpty(){
+        assertNull(MaxOfOddCalculatorUtil.maxOfOddNumber(null));
+        assertNull(MaxOfOddCalculatorUtil.maxOfOddNumber(List.of()));
+    }
+
+    @Test
+    @DisplayName("MaxOdd result is correct")
+    void longestStringTest(){
+        assertEquals(31, MaxOfOddCalculatorUtil.maxOfOddNumber(List.of(-8,-4,2,3,6,8,31,31)));
+        assertEquals(1, MaxOfOddCalculatorUtil.maxOfOddNumber(List.of(-1265,-50,0,1 )));
+    }
+}
