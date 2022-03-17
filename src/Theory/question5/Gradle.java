3b08254028b880a5f8b755d22ab0fecef749ef8c
Benedek Maveev <[bematv@ttu.ee]>
2022-03-17 15:45:57 +0200

    Add theory part to the project

---
+++ src/Theory/question5/Gradle.java
@@ -0,0 +1,26 @@
+package ee.taltech.a_theory2.question5;
+
+public class Gradle {
+
+    //todo
+    // Since lesson 1 we've been using Gradle
+
+    //todo A
+    // In your words (do not copy wiki definitions)
+    // What is gradle?
+    // Answer: Gradle is a build automation tool which allows you to build any kind of software. Gradle helps Java to compile with its build automation, and handles dependencies and packages.
+
+    //todo B
+    // What is gradle wrapper? Why are we using it instead of gradle?
+    // Answer: Gradle wrapper is an interface to build Android project. With gradle wrapper you can use gradle features for builds without installing Gradle.
+
+    //todo C
+    // Name 2 different benefits or reasons for using gradle
+    // 1 It gives flexibility to build software.
+    // 2 Open-source
+
+    //todo D
+    // Name at least 1 alternative to gradle
+    // 1 Jenkins
+
+}
